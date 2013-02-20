d70a33f6f85bb8cdc23c9a72913f24038275c75a
Victor Lowther <victor.lowther@gmail.com>
2013-02-20 12:01:46 -0600

    Add metadata.rb for bind9 cookbook.

---
+++ chef/cookbooks/bind9/metadata.rb
@@ -0,0 +1,7 @@
+maintainer        "Dell, Inc"
+maintainer_email  "crowbar@dell.com"
+license           "Apache 2.0"
+description       "Manages Bind9 for Crowbar"
+long_description  "Managed Bind9 for Crowbar"
+version           "2.0.o
+

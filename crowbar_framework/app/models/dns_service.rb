5c15ed30919aab25646e2484bfc4075300df5b06
Rick Salevsky <rsalevsky@suse.de>
2015-04-28 13:42:57 +0200

    Allow disabling the DNS-Server
    This change is needed to allow using only external dns-servers.

--- crowbar_framework/app/models/dns_service.rb
+++ crowbar_framework/app/models/dns_service.rb
@@ -50,7 +50,12 @@ class DnsService < ServiceObject
   end
 
   def validate_proposal_after_save proposal
-    validate_at_least_n_for_role proposal, "dns-server", 1
+    server_role = proposal["deployment"]["dns"]["elements"]["dns-server"]
+    nameservers = proposal["attributes"]["dns"]["nameservers"]
+
+    if server_role.blank? && nameservers.blank?
+      validation_error("At least one nameserver or one node with the dns-server role must be specified.")
+    end
 
     super
   end
@@ -91,38 +96,40 @@ class DnsService < ServiceObject
     return if all_nodes.empty?
 
     tnodes = role.override_attributes["dns"]["elements"]["dns-server"]
-    nodes = tnodes.map {|n| NodeObject.find_node_by_name n}
 
-    # electing master dns-server
-    master = nil
-    admin = nil
-    nodes.each do |node|
-      if node[:dns][:master]
-        master = node
-        break
-      elsif node.admin?
-        admin = node
+    if !tnodes.blank?
+      nodes = tnodes.map {|n| NodeObject.find_node_by_name n}
+      # electing master dns-server
+      master = nil
+      admin = nil
+      nodes.each do |node|
+        if node[:dns][:master]
+          master = node
+          break
+        elsif node.admin?
+          admin = node
+        end
       end
-    end
-    if master.nil?
-      unless admin.nil?
-        master = admin
-      else
-        master = nodes.first
+      if master.nil?
+        unless admin.nil?
+          master = admin
+        else
+          master = nodes.first
+        end
       end
-    end
 
-    slave_ips = nodes.map {|n| n[:crowbar][:network][:admin][:address]}
-    slave_ips.delete(master[:crowbar][:network][:admin][:address])
-    slave_nodes = tnodes.dup
-    slave_nodes.delete(master.name)
+      slave_ips = nodes.map {|n| n[:crowbar][:network][:admin][:address]}
+      slave_ips.delete(master[:crowbar][:network][:admin][:address])
+      slave_nodes = tnodes.dup
+      slave_nodes.delete(master.name)
 
-    nodes.each do |node|
-      node.set[:dns][:master_ip] = master[:crowbar][:network][:admin][:address]
-      node.set[:dns][:slave_ips] = slave_ips
-      node.set[:dns][:slave_names] = slave_nodes
-      node.set[:dns][:master] = (master.name == node.name)
-      node.save
+      nodes.each do |node|
+        node.set[:dns][:master_ip] = master[:crowbar][:network][:admin][:address]
+        node.set[:dns][:slave_ips] = slave_ips
+        node.set[:dns][:slave_names] = slave_nodes
+        node.set[:dns][:master] = (master.name == node.name)
+        node.save
+      end
     end
 
     @logger.debug("DNS apply_role_pre_chef_call: leaving")

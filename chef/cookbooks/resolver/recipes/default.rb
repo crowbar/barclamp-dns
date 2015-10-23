186329f32f713404e5714ccf618dd95b40cddfe7
Vincent Untz <vuntz@suse.com>
2015-10-23 11:42:31 +0200

    resolver: Reload dnsmasq after DNS change if node is dns-server
    Since dnsmasq is caching its data, we need to reload the local dnsmasq
    in case there's a change in the DNS config (when we run bind). This
    makes it possible to have a node get a new IP address and have us still
    be able to resolve to the correct IP address.
    
    We use a delayed notification because bind is also using a delayed one,
    so it makes no sense to reload dnsmasq before bind.
    
    Backport of https://github.com/crowbar/crowbar-core/pull/98

--- chef/cookbooks/resolver/recipes/default.rb
+++ chef/cookbooks/resolver/recipes/default.rb
@@ -56,6 +56,10 @@ unless node[:platform] == "windows"
       action [:enable, :start]
       subscribes :restart, "template[/etc/dnsmasq.conf]"
       subscribes :restart, "template[/etc/resolv-forwarders.conf]"
+      if node["roles"].include?("dns-server")
+        # invalidate dnsmasq cache if local zone changes
+        subscribes :reload, "template[/etc/bind/db.#{node[:dns][:domain]}]"
+      end
     end
 
     dns_list = dns_list.insert(0, "127.0.0.1").take(3)
